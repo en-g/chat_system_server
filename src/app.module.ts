3ed127251c7361fc992d631468c28f4cdb585155
Eng <1820591474@qq.com>
2023-01-13 15:29:28 +0800

    feat: 连接数据库和抽离环境变量

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { SequelizeModule } from '@nestjs/sequelize'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    // 导入 env 环境变量
+    ConfigModule.forRoot(),
+    // 连接数据库
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common'
+import { QueryTypes } from 'sequelize'
+import { Sequelize } from 'sequelize-typescript'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  constructor(private sequelize: Sequelize) {}
+  async getHello() {
+    const friendGroupSelect = `SELECT * FROM userInfo WHERE user_id = 1`
+    const result = await this.sequelize.query(friendGroupSelect, {
+      type: QueryTypes.SELECT,
+    })
+    return result
   }
 }
