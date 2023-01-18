c970f2895a0e005c59b2424cb513f99d0d1b3ba3
Eng <1820591474@qq.com>
2023-01-18 22:31:22 +0800

    feat: 完成登录时的用户查询

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common'
+import { Controller, Post, Get, Query, Request, UseGuards } from '@nestjs/common'
 import { AuthService } from '../auth/auth.service'
 import { LocalAuthGuard } from '../auth/local-auth.guard'
+import { LoginInfo } from './dto/login.dto'
+import { LoginService } from './login.service'
 
 @Controller('login')
 export class LoginController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService, private readonly loginService: LoginService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post()
   async login(@Request() req) {
     return await this.authService.login(req.user)
+  }
+
+  // 查询用户是否存在
+  @Get('search')
+  async loginSearch(@Query() info: LoginInfo) {
+    return await this.loginService.loginSearch(info)
   }
 }
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common'
+import { QueryTypes } from 'sequelize'
+import { Sequelize } from 'sequelize-typescript'
+import { LoginInfo } from './interface/login.interface'
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+  constructor(private sequelize: Sequelize) {}
+
+  async loginSearch(info: LoginInfo) {
+    const userSelect = `SELECT id, username, password, email FROM users WHERE (username = :username OR email = :username) AND password = :password`
+    const result = await this.sequelize.query(userSelect, {
+      replacements: { ...info },
+      type: QueryTypes.SELECT,
+    })
+    return !!result.length
+  }
+}
