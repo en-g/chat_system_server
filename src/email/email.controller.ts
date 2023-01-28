516d39491adad8351717669998aad1d4bb625b50
Eng <1820591474@qq.com>
2023-01-28 14:32:13 +0800

    feat: 补全注释

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -6,11 +6,13 @@ import { EmailService } from './email.service'
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
+  // 发送修改密码的验证码
   @Get('update')
   async sendUpdatePassVerificationCode(@Query() code: UploadPassWordCode) {
     return await this.emailService.sendUpdatePassVerificationCode(code)
   }
 
+  // 发送用户注册的验证码
   @Get('register')
   async sendUserRegisterVerificationCode(@Query() code: RegisterCode) {
     return await this.emailService.sendUserRegisterVerificationCode(code)
